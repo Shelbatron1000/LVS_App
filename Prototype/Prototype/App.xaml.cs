1f325a273f5419e9f0e46738246404d4aca0fcb4
Shelbatron1000 <shelbatron1000@gmail.com>
2017-11-28 11:34:11 -0500

    Changed Main Text

--- Prototype/Prototype/App.xaml.cs
+++ Prototype/Prototype/App.xaml.cs
@@ -20,7 +20,7 @@ namespace Prototype
                     Children = {
             new Label {
                HorizontalTextAlignment = TextAlignment.Center,
-               Text = "Welcome to Xamarin Forms!"
+               Text = "Welcome to Xamarin Forms! Let's Begin..."
             }
          }
 
